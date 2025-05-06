4de0b2b640734961eae07fbe3431aa8409b96cac
Samiul Hasan <samiulxhasan650@gmail.com>
2025-05-07 03:44:39 +0600

    Dashboard page

---
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DashboardPage = () => {
+    return (
+        <div>
+            Welcome to hospital management system
+        </div>
+    );
+};
+
+export default DashboardPage;
